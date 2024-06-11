f163d6c4242bec254e0f0a406e003e8f592582c5
Andrea Cardinale <mirthis@gmail.com>
2024-06-11 16:40:18 +0100

    update dashboard icons to use lucide icons

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -16,6 +16,7 @@ import { CalendarDateRangePicker } from "@/components/ui/date--range-picker";
 import { OverviewChart } from "@/components/results/OverviewChart";
 import DataTable from "@/components/results/DataTable";
 import KPICard from "@/components/results/KPICard";
+import { Activity, CreditCard, DollarSign, Users } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -49,79 +50,28 @@ export default function DashboardPage() {
                   value="$45,231.89"
                   text="+20.1% from last month"
                   icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
+                    <DollarSign className="text-muted-foreground h-5 w-5" />
                   }
                 />
                 <KPICard
                   name="Subscriptions"
                   value="+2350"
                   text="+180.1% from last month"
-                  icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                      <circle cx="9" cy="7" r="4" />
-                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                    </svg>
-                  }
+                  icon={<Users className="text-muted-foreground h-5 w-5" />}
                 />
                 <KPICard
                   name="Sales"
                   value="+12,234"
                   text="+19% from last month"
                   icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <rect width="20" height="14" x="2" y="5" rx="2" />
-                      <path d="M2 10h20" />
-                    </svg>
+                    <CreditCard className="text-muted-foreground h-5 w-5" />
                   }
                 />
                 <KPICard
                   name="Active Now"
                   value="+573"
                   text="+201 since last hour"
-                  icon={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
-                  }
+                  icon={<Activity className="text-muted-foreground h-5 w-5" />}
                 />
               </div>
               <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-6">
